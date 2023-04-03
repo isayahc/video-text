826d94b797adcb38522f3e8621885c1eb1bbdbe6
isayahc <isayahculbertson@gmail.com>
2023-04-03 17:44:05 -0400

    developing example code

--- sample.py
+++ sample.py
@@ -1,8 +1,4 @@
 from main import add_text_to_video
-
-input_file = 'ocean-65560.mp4'
-output_file = 'ocean_output_video3.mp4'
-
 text_1 = "The West Indies is a subregion of North America, surrounded by the North Atlantic Ocean and the Caribbean Sea, which comprises 13 independent island countries, 18 dependencies, and three archipelagos: the Greater Antilles, the Lesser Antilles, and the Lucayan Archipelago. "
 text_2 =  text_1 + "The subregion includes all the islands in the Antilles, plus The Bahamas and the Turks and Caicos Islands, which are in the North Atlantic Ocean. "
 text_3 = text_2 + (
@@ -11,46 +7,53 @@ text_3 = text_2 + (
         "These countries are geographically distinct from the three main island groups, but culturally related.")
 
 
-text_instructions = [
-        {
-            "start": 0,
-            "end": 5,
-            "text": text_1,
-            "font_file_location": "Roboto/Roboto-Regular.ttf",
-            "font_size": 30,
-            "text_body_position": (50, 50),
-            "text_color": (255, 255, 255),
-            "stroke": 2,
-            "stroke_color": (0, 0, 0),
-            "shadow": (4, 4),
-            "shadow_color": (0, 0, 0)
-        },
-        {
-            "start": 5,
-            "end": 10,
-            "text": text_2,
-            "font_file_location": "Roboto/Roboto-Regular.ttf",
-            "font_size": 30,
-            "text_body_position": (50, 50),
-            "text_color": (255, 255, 255),
-            "stroke": 2,
-            "stroke_color": (0, 0, 0),
-            "shadow": (4, 4),
-            "shadow_color": (0, 0, 0)
-        },
-        {
-            "start": 10,
-            "end": 15.6,
-            "text": text_3,
-            "font_file_location": "Roboto/Roboto-Regular.ttf",
-            "font_size": 30,
-            "text_body_position": (50, 50),
-            "text_color": (255, 255, 255),
-            "stroke": 2,
-            "stroke_color": (0, 0, 0),
-            "shadow": (4, 4),
-            "shadow_color": (0, 0, 0)
-        }
-    ]
+input_file = 'input_video.mp4'
+output_file = 'output_video2.mp4'
 
-add_text_to_video(input_file, output_file, text_instructions)
+font_file = 'assets\Roboto\Roboto-Bold.ttf'
+font_size = 50
+stroke_width = 1
+
+text_instructions = [
+    {
+        "start": 0,
+        "end": 5,
+        "text": text_1,
+        "font_file_location": font_file,
+        "font_size": font_size,
+        "text_body_position": (50, 50),
+        "text_color": (0, 0, 0),
+        "stroke": stroke_width,
+        "stroke_color": (0, 0, 0),
+        "shadow": (4, 4),
+        "shadow_color": (0, 0, 0)
+    },
+    {
+        "start": 5,
+        "end": 10,
+        "text": text_2,
+        "font_file_location": font_file,
+        "font_size": font_size,
+        "text_body_position": (50, 50),
+        "text_color": (0, 0, 0),
+        "stroke": stroke_width,
+        "stroke_color": (0, 0, 0),
+        "shadow": (4, 4),
+        "shadow_color": (0, 0, 0)
+    },
+    {
+        "start": 10,
+        "end": 15,
+        "text": text_3,
+        "font_file_location": font_file,
+        "font_size": font_size,
+        "text_body_position": (50, 50),
+        "text_color": (0, 0, 0),
+        "stroke": stroke_width,
+        "stroke_color": (0, 0, 0),
+        "shadow": (4, 4),
+        "shadow_color": (0, 0, 0)
+    }
+]
+
+add_text_to_video(input_file, output_file, text_instructions,overwrite=True)
